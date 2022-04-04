37f93b89667bdeb0632dcf3a59aa0a6a31754459
yohann79 <yohann.borlot@gmail.com>
2022-04-04 16:23:25 +0200

    re creation du projet

--- src/main/java/eni/porjetEnchere/PorjetEnchereApplication.java
+++ src/main/java/eni/porjetEnchere/PorjetEnchereApplication.java
@@ -1,13 +1,13 @@
-package core;
-
+package eni.porjetEnchere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main{
+public class PorjetEnchereApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-			
+		SpringApplication.run(PorjetEnchereApplication.class, args);
 	}
+
 }
