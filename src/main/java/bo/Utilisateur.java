adf1c1d62a75dc69a86afcbc65e37a182f9250c3
yohann79 <yohann.borlot@gmail.com>
2022-04-05 11:48:19 +0200

    BO: Utilisateur + gitignore

---
+++ src/main/java/bo/Utilisateur.java
@@ -0,0 +1,16 @@
+package bo;
+
+public class Utilisateur {
+	int noUtilisateur;
+	String nom;
+	String prenom;
+	String pseudo;
+	String email;
+	String telephone;
+	String rue;
+	String code_postal;
+	String ville;
+	String mot_de_passe;
+	int credit;
+	Boolean administrateur;
+}
