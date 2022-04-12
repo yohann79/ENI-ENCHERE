dae0c399b2471699d55138ac6cd0f0c65f29cec4
Cerise <cerisepelush@gmail.com>
2022-04-12 12:16:21 +0200

    Servlet Error

---
+++ src/main/java/bll/ArticleVenduError.java
@@ -0,0 +1,7 @@
+package bll;
+
+public class ArticleVenduError extends Exception {
+    public ArticleVenduError(String error) {
+        super(error);
+    }
+}
--- src/main/java/ihm/VendreServlet.java
+++ src/main/java/ihm/VendreServlet.java
@@ -1,12 +1,19 @@
 package ihm;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bll.ArticlesManager;
+import bll.ArticleVenduError;
+import bo.ArticleVendu;
+import bo.Retrait;
 
 @WebServlet("/Vendre")
 public class VendreServlet extends HttpServlet {
@@ -25,7 +32,47 @@ public class VendreServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        super.doPost(req, resp);
-    }
 
-}
+        String nomArticle = req.getParameter("nomArticle");
+        String description = req.getParameter("description");
+        int categorie = Integer.parseInt(req.getParameter("categorie"));
+        int prixInitial = Integer.parseInt(req.getParameter("prixInitial"));
+        Date dateDebutEnchere = Date.parseDate(req.getParameter("dateDebutEnchere"));
+        Date dateFinEnchere = Date.parseDate(req.getParameter("dateFinEnchere"));
+
+        String rue = req.getParameter("rue");
+        String codePostal = req.getParameter("codePostal");
+        String ville = req.getParameter("ville");
+
+        if (nomArticle.isEmpty() || description.isEmpty() || categorie.isEmpty() || prixInitial.isEmpty() ||
+                dateDebutEnchere.isEmpty() || dateFinEnchere.isEmpty() || rue.isEmpty() || codePostal.isEmpty() || ville.isEmpty() ){
+            req.setAttribute("error", "les champs ne doivent pas etre vide");
+            req.getRequestDispatcher("WEB-INF/vendreError.jsp").forward(req, resp);
+        } else {
+
+            ArticleVendu ArticleVendu = new ArticleVendu();
+            ArticleVendu.setNomArticle(nomArticle);
+            ArticleVendu.setDescription(description);
+            ArticleVendu.setNoCategorie(categorie);
+            ArticleVendu.setPrixInitial(prixInitial);
+            ArticleVendu.setDateDebutEnchere(dateDebutEnchere);
+            ArticleVendu.setDateFinEnchere(dateFinEnchere);
+
+            Retrait Retrait = new Retrait();
+
+            Retrait.setRue(rue);
+            Retrait.setCode_postal(codePostal);
+            Retrait.setVille(ville);
+
+            try {
+                
+                } catch ( ArticleVenduError e) {
+                    req.setAttribute("error", e.getMessage());
+                    req.getRequestDispatcher("WEB-INF/vendreError.jsp").forward(req, resp);
+                }
+            } catch (SQLException e) {
+                req.setAttribute("error", e.getMessage());
+                req.getRequestDispatcher("WEB-INF/vendreError.jsp").forward(req, resp);
+            }
+        }
+}}
