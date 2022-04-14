4068b2d97d678d08f2ff4f272d18b36109ade355
Cerise <cerisepelush@gmail.com>
2022-04-14 19:35:32 +0200

    minor

--- src/main/java/bo/Utilisateur.java
+++ src/main/java/bo/Utilisateur.java
@@ -1,155 +1,155 @@
 package bo;
 
 public class Utilisateur {
-	private int no_utilisateur;
-	private String pseudo;
-	private String nom;
-	private String prenom;
-	private String email;
-	private String telephone;
-	private String rue;
-	private String code_postal;
-	private String ville;
-	private String mot_de_passe;
-	private int credit;
-	private Boolean administrateur;
+    private int no_utilisateur;
+    private String pseudo;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private String code_postal;
+    private String ville;
+    private String mot_de_passe;
+    private int credit;
+    private Boolean administrateur;
 
-	/**
-	 * all arg constructor
-	 * 
-	 * @param noUtilisateur
-	 * @param nom
-	 * @param prenom
-	 * @param pseudo
-	 * @param email
-	 * @param telephone
-	 * @param rue
-	 * @param code_postal
-	 * @param ville
-	 * @param mot_de_passe
-	 * @param credit
-	 * @param administrateur
-	 */
-	public Utilisateur(int no_utilisateur, String nom, String prenom, String pseudo, String email, String telephone,
-			String rue, String code_postal, String ville, String mot_de_passe, int credit, Boolean administrateur) {
+    /**
+     * all arg constructor
+     * 
+     * @param noUtilisateur
+     * @param nom
+     * @param prenom
+     * @param pseudo
+     * @param email
+     * @param telephone
+     * @param rue
+     * @param code_postal
+     * @param ville
+     * @param mot_de_passe
+     * @param credit
+     * @param administrateur
+     */
+    public Utilisateur(int no_utilisateur, String nom, String prenom, String pseudo, String email, String telephone,
+            String rue, String code_postal, String ville, String mot_de_passe, int credit, Boolean administrateur) {
 
-		this.no_utilisateur = no_utilisateur;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.pseudo = pseudo;
-		this.email = email;
-		this.telephone = telephone;
-		this.rue = rue;
-		this.code_postal = code_postal;
-		this.ville = ville;
-		this.mot_de_passe = mot_de_passe;
-		this.credit = credit;
-		this.administrateur = administrateur;
-	}
+        this.no_utilisateur = no_utilisateur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.pseudo = pseudo;
+        this.email = email;
+        this.telephone = telephone;
+        this.rue = rue;
+        this.code_postal = code_postal;
+        this.ville = ville;
+        this.mot_de_passe = mot_de_passe;
+        this.credit = credit;
+        this.administrateur = administrateur;
+    }
 
-	public Utilisateur() {
+    public Utilisateur() {
 
-	}
+    }
 
-	/**
-	 * 
-	 * Getter and Setter for all field
-	 */
+    /**
+     * 
+     * Getter and Setter for all field
+     */
 
-	public int getNo_utilisateur() {
-		return no_utilisateur;
-	}
+    public int getNo_utilisateur() {
+        return no_utilisateur;
+    }
 
-	public void setNo_utilisateur(int no_utilisateur) {
-		this.no_utilisateur = no_utilisateur;
-	}
+    public void setNo_utilisateur(int no_utilisateur) {
+        this.no_utilisateur = no_utilisateur;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
-	public String getPseudo() {
-		return pseudo;
-	}
+    public String getPseudo() {
+        return pseudo;
+    }
 
-	public void setPseudo(String pseudo) {
-		this.pseudo = pseudo;
-	}
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getTelephone() {
-		return telephone;
-	}
+    public String getTelephone() {
+        return telephone;
+    }
 
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
-	public String getRue() {
-		return rue;
-	}
+    public String getRue() {
+        return rue;
+    }
 
-	public void setRue(String rue) {
-		this.rue = rue;
-	}
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
 
-	public String getCode_postal() {
-		return code_postal;
-	}
+    public String getCode_postal() {
+        return code_postal;
+    }
 
-	public void setCode_postal(String code_postal) {
-		this.code_postal = code_postal;
-	}
+    public void setCode_postal(String code_postal) {
+        this.code_postal = code_postal;
+    }
 
-	public String getVille() {
-		return ville;
-	}
+    public String getVille() {
+        return ville;
+    }
 
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
 
-	public String getMot_de_passe() {
-		return mot_de_passe;
-	}
+    public String getMot_de_passe() {
+        return mot_de_passe;
+    }
 
-	public void setMot_de_passe(String mot_de_passe) {
-		this.mot_de_passe = mot_de_passe;
-	}
+    public void setMot_de_passe(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
 
-	public int getCredit() {
-		return credit;
-	}
+    public int getCredit() {
+        return credit;
+    }
 
-	public void setCredit(int credit) {
-		this.credit = credit;
-	}
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
 
-	public Boolean getAdministrateur() {
-		return administrateur;
-	}
+    public Boolean getAdministrateur() {
+        return administrateur;
+    }
 
-	public void setAdministrateur(Boolean administrateur) {
-		this.administrateur = administrateur;
-	}
+    public void setAdministrateur(Boolean administrateur) {
+        this.administrateur = administrateur;
+    }
 
 }
